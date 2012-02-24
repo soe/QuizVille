3f71902e16cd019994b4d135f9570bca81f1ea29
Soe <soe@soe.im>
2012-02-24 04:38:47 +0100

    go

--- public/application.js
+++ public/application.js
@@ -1,5 +1,6 @@
 $(function() {
   var channel = $('#channel').text();
+  var username = $('#username').text();
 
   var cometd = new Faye.Client('http://quizville-cometd.herokuapp.com/cometd', {timeout: 120});
   cometd.disable('websocket');
@@ -7,8 +8,10 @@ $(function() {
   if(channel) {
     var subscription = cometd.subscribe(channel, function(message) {
       console.log(message);
-      $('#answers').append(message);
-      alert(message);
+      m = message.sobject;
+      
+      $('<div class="answer" id="'+ m.Id +'"><span class="user">'+ username +'</span><span class="language '+ m.Language__c +'">'+ m.Language__c +'</span><span class="correct '+ m.Is_Correct__c +'">'+ m.Is_Correct__c  +'</span><span class="time">'+ m.Time__c  +'</span></div>')
+        .appendTo('#answers');
     });
     subscription.callback(function() {
       console.log('Subscription is now active!');
@@ -26,6 +29,6 @@ $(function() {
   };
   
   if(channel == '/q/demo') {
-    setInterval (demo_publish, 20000);
+    setInterval (demo_publish, 10000);
   }
 });
